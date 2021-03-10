e1cb1d2ab711e88bd5455474b8ee61380357cfc4
aldyrifaldi <aldyrifaldi060621@gmail.com>
2021-03-10 10:30:48 +0700

    added enum response code

---
+++ enums/response-code.js
@@ -0,0 +1,20 @@
+
+// success
+exports.OK = 200
+exports.CREATED = 201
+exports.ACCEPTED = 202
+exports.NO_CONTENT = 204
+
+// client error
+exports.BAD_REQUEST = 400
+exports.UNAUTHORIZED = 401
+exports.FORBIDDEN = 403
+exports.NOT_FOUND = 404
+exports.METHOD_NOT_ALLOWED = 405
+exports.REQUEST_TIMEOUT = 408
+exports.UNPROCESSABLE_ENTITY = 422
+
+// server error
+exports.INTERNAL_SERVER_ERROR = 500
+exports.BAD_GATEWAY = 502
+exports.SERVICE_UNAVAILABLE = 503   
